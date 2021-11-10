d0705edfcc4d408299887f0b6261d10bf4edf16c
William Ng <84236898+williamng95@users.noreply.github.com>
2021-11-10 11:55:16 +0800

    npm tett

---
+++ test.js
@@ -0,0 +1,9 @@
+// code snippet that the students will run after installing node.js
+
+let factorial = 1;
+let i = 2;
+while (i < 10) {
+  factorial = factorial * i;
+  i++;
+}
+console.log(factorial);
