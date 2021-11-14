ac778b59602823cca998ce3bcce7f09f1ed61266
William Ng <84236898+williamng95@users.noreply.github.com>
2021-11-14 17:55:12 +0800

    express template

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,5 @@
 # backend in express
+
+index.js: basic hello world test.
+
+created with npx express-generator, express my-app
--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,12 @@
 // placeholder js file
-import express from "express";
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
