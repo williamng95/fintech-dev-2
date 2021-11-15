ee34a8c48f3651d54e5a33a49c1e958b8a215001
William Ng <84236898+williamng95@users.noreply.github.com>
2021-11-15 18:13:46 +0800

    placeholder query to postgresql db

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+require('dotenv').config()
+
+const { Client } = require('pg')
+const config ={ssl: {
+  rejectUnauthorized: false}}
+const client = new Client(config)
+client.connect()
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  client.query("select * from gb.user", (err,qres)=>{
+    res.setHeader('Content-Type', 'application/json')
+    res.json(qres.rows)
+    client.end
+  })
 });
 
 module.exports = router;
