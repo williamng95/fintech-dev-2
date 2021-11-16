4e0530e275ffa623056c18a86995b0fc0d940e0d
William Ng <84236898+williamng95@users.noreply.github.com>
2021-11-16 23:55:43 +0800

    frontend auth0 login

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton'
 
 function App() {
   return (
     <div className="App">
+      
+      <LoginButton />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- frontend/myapp/src/index.js
+++ frontend/myapp/src/index.js
@@ -1,15 +1,23 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
+console.log(window.location.origin)
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTHURL}
+    clientId={process.env.REACT_APP_AUTHCLIENT}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
