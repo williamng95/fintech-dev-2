45cfe3bf6922824953e0172d18b925020a37f82b
William Ng <84236898+williamng95@users.noreply.github.com>
2021-11-16 13:08:47 +0800

    implement auth0 on backend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,6 +9,18 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
+const auth_config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTHSECRET,
+  baseURL: 'http://localhost:3000',
+  clientID: 'obOUznVsGE3nYOEiiop5t0xt352zeZQm',
+  issuerBaseURL: 'https://dev-kj-18osw.us.auth0.com'
+};
 
 var app = express();
 
@@ -19,10 +33,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(auth(auth_config));
 
-app.use('/', indexRouter);
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+app.use('/api',requiresAuth(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
