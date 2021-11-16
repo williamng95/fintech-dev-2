14d4b3c6d45549854a460a54592dd5bf220ddf65
William Ng <84236898+williamng95@users.noreply.github.com>
2021-11-17 00:45:09 +0800

    prettify with bootstrap and build logout

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginButton from './components/LoginButton'
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       
-      <LoginButton />
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- frontend/myapp/src/index.js
+++ frontend/myapp/src/index.js
@@ -1,18 +1,19 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
 
-console.log(window.location.origin)
 ReactDOM.render(
+  
   <Auth0Provider
     domain={process.env.REACT_APP_AUTHURL}
     clientId={process.env.REACT_APP_AUTHCLIENT}
     redirectUri={window.location.origin}
   >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <App />
   </Auth0Provider>,
   document.getElementById("root")
